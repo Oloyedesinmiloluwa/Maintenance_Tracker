cb192e37620f90d05b671318b1c11f373743d3a8
Sinmiloluwa <sinmiloluwasunday@yahoo.com>
2018-05-18 16:24:06 +0100

    [Feature 157689022] add get a request

--- server/src/controllers/requestController.js
+++ server/src/controllers/requestController.js
@@ -9,7 +9,7 @@ export default class requestController {
    * @param {Object} req - The client request object.
    * @returns {Object} The selected request
    */
-  static selectrequest(req) {
+  static selectRequest(req) {
     const id = parseInt(req.params.requestId, 10);
     if (!data[id]) return null;
     return data[id];
@@ -31,5 +31,15 @@ export default class requestController {
    */
   static getAll(req, res) {
     return res.status(200).send(data);
+  }
+  /**
+   * This method gets a single request.
+   * @param {Object} req - client request Object
+   * @param {Object} res - Server response Object
+   * @returns {Object} request
+   */
+  static getARequest (req, res) {
+    if (requestController.selectRequest(req) === null) return res.status(404).send({ message: 'Invalid ID' });
+    res.status(200).send(requestController.selectRequest(req));
   }
 }
--- server/src/model/data.js
+++ server/src/model/data.js
@@ -1,7 +1,17 @@
 export const data = [{
+  id: 0,
   title: "Faulty WC",
   description: "The first WC in the rest room is not working",
   status: "approved",
   date: "12-12-2018",
   image: ""
-}]
+},
+{
+  id: 1,
+  title: "Faulty AVR",
+  description: "The last AVR is not working",
+  status: "approved",
+  date: "12-12-2018",
+  image: ""
+}
+]
