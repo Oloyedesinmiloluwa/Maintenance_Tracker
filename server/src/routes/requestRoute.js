be1e018768876abad1c016a920f3c9f9941850b7
Oloyedesinmiloluwa <sinmiloluwasunday@yahoo.com>
2018-05-23 10:25:03 +0100

    [Feature #157723434] persist create request

--- server/src/routes/requestRoute.js
+++ server/src/routes/requestRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import requestController from '../controllers/requestController';
+import requestValidator from '../middlewares/requestValidator';
 
 const requestRoute = express();
 requestRoute.use(bodyParser.json());
 requestRoute.use(bodyParser.urlencoded({ extended: false }));
 requestRoute.route('/users/requests')
-  .get(requestController.getAll);
+  .get(requestController.getAll)
+  .post(requestValidator.addRequest, requestController.addRequest);
 requestRoute.route('/users/requests/:requestId')
-  .get(requestController.getARequest)
+  .get(requestController.getARequest);
 export default requestRoute;
