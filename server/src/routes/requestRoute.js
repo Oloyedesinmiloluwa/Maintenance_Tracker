76f56eab0e60862767f578319627f4676d08457a
Oloyedesinmiloluwa <sinmiloluwasunday@yahoo.com>
2018-05-24 13:40:22 +0100

    [bug #157838837] secure endpoints for user requests

--- server/src/routes/requestRoute.js
+++ server/src/routes/requestRoute.js
@@ -2,14 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import requestController from '../controllers/requestController';
 import requestValidator from '../middlewares/requestValidator';
+import auth from '../middlewares/auth';
 
 const requestRoute = express();
 requestRoute.use(bodyParser.json());
 requestRoute.use(bodyParser.urlencoded({ extended: false }));
 requestRoute.route('/users/requests')
-  .get(requestController.getAll)
-  .post(requestValidator.addRequest, requestController.addRequest);
+  .get(auth, requestController.getAll)
+  .post(requestValidator.addRequest, auth, requestController.addRequest);
 requestRoute.route('/users/requests/:requestId')
-  .get(requestController.getARequest)
-  .put(requestValidator.modifyRequest, requestController.modifyRequest);
+  .get(auth, requestController.getARequest)
+  .put(auth, requestValidator.modifyRequest, requestController.modifyRequest);
 export default requestRoute;
