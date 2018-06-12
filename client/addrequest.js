c841fdd8bd7eeb4584cd32799db979d575321771
Sinmiloluwa <sinmiloluwasunday@yahoo.com>
2018-06-12 12:34:03 +0100

    feat(client): implement status update
    - enable admin approve request feature
    - enable admin disapprove request feature
    - enable admin resolve request feature
    - improve app navigation
    
    [Finishes #157997789]

--- client/addrequest.js
+++ client/addrequest.js
@@ -3,11 +3,20 @@ const titleInput = document.getElementById('title-input');
 const descriptionInput = document.getElementById('description-input');
 const categoryInput = document.getElementById('category-input');
 const messageText = document.querySelector('#messageText');
+const userWelcomeText = document.querySelector('#userWelcomeText');
 const navLinks = document.querySelectorAll('ul a');
+let isEditRequest = false;
 
+navLinks[2].addEventListener('click', (event) => {
+  localStorage.setItem('token', null);
+  localStorage.setItem('userName', ' ');
+});
 window.addEventListener('load', (event) => { 
   navLinks[2].textContent = 'Sign Out';
+  navLinks[2].href = 'index.html';
+  userWelcomeText.innerHTML += ` ${localStorage.getItem('userName')}`;
   if (localStorage.getItem('editRequest') === 'started') {
+    isEditRequest = true;
     const request = {
       title: localStorage.getItem('requestBufferTitle'),
       description: localStorage.getItem('requestBufferDescription'),
@@ -16,6 +25,7 @@ window.addEventListener('load', (event) => {
     titleInput.value = request.title;
     descriptionInput.value = request.description;
     categoryInput.value = request.category;
+    localStorage.setItem('editRequest', 'ended');
   }
 });
 button.addEventListener('click', (event) => {
@@ -27,7 +37,7 @@ button.addEventListener('click', (event) => {
   };
   let url = 'https://m-tracker.herokuapp.com/api/v1/users/requests';
   let httpMethod = 'POST';
-  if (localStorage.getItem('editRequest') === 'started') {
+  if (isEditRequest) {
     url = `https://m-tracker.herokuapp.com/api/v1/users/requests/${localStorage.getItem('requestId')}`;
     httpMethod = 'PUT';
   }
@@ -48,6 +58,6 @@ button.addEventListener('click', (event) => {
       else {
         messageText.textContent = data.message;
       }
-      localStorage.setItem('editRequest', 'ended');
+      isEditRequest = false;
     });
 });
