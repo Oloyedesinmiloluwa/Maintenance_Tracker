bebc5ce508d017cfe92fbdd1b814a077e5c35e8f
Sinmiloluwa Oloyede <sinmiloluwasunday@yahoo.com>
2018-10-02 14:48:57 +0100

    chore(setup): setup webpack
    - add webpack config file
    - enable image bundling
    - enable css bundling
    - transpile with babel
    
    [Finishes #160908795]

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const APP_DIR = path.resolve(__dirname, 'react');
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: `${APP_DIR}/public/index.html`,
+});
+const config = {
+  mode: 'production',
+  entry: `${APP_DIR}/index.jsx`,
+  output: {
+    publicPath: '/',
+    path: `${BUILD_DIR}`,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: APP_DIR,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
+};
+
+module.exports = config;
