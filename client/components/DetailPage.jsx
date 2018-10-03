ae5ea48862797ba0da5d2081aba9c45aba3cb469
Sinmiloluwa Oloyede <sinmiloluwasunday@yahoo.com>
2018-10-03 17:09:38 +0100

    feat(view-request): add get request feature
    - add show request component
    - add get single request thunk
    - add set current request action
    - enable delete request
    
    [Delivers #160710951 #160710968]

---
+++ client/components/DetailPage.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import image from '../../vanilla/assets/image/repair2.png';
+import { setStatus } from '../helpers';
+import MessageText from './MessageText';
+const formatDate = (dated) => {
+  const date = new Date(dated);
+  return date.toLocaleDateString();
+}
+
+export default ({ request, isAdmin, handleClick, handleChange, dropDownValue, message }) => (
+  <div>
+    <div>
+      <div id="detail-card" className="show-detail-card">
+        <div className="display-card">
+          <img src={request.image || image} />
+          <div className="card-content">
+            <h3 id="title-label">{request.title}&nbsp;&nbsp;<i className={setStatus(request.status)}></i></h3>
+          </div>
+        </div>
+      </div>
+      <div className="addrequest-form">
+        <p className="description-label">Description:</p> <p id="description-label" className="description-p">{request.description}</p>
+        <p className="description-label">Category:</p> <p id="category-label" className="description-p">{request.category}</p>
+        <p className="description-label">Date:</p><p id="date-label" className="description-p">{formatDate(request.dated)}</p>
+      </div>
+      <MessageText message={message} />
+      <div className="description-btn">
+        {isAdmin && <select value={dropDownValue} onChange={handleChange}>
+          <option value="approve">Approve</option>
+          <option value="disapprove">Disapprove</option>
+          <option value="resolve">Resolve</option>
+        </select>}
+        <button onClick={handleClick} id="edit-btn" >{isAdmin ? "Update Status" : "Edit"}</button>
+        {!isAdmin && <button onClick={handleClick} id="delete-btn" >Delete</button>}
+      </div>
+    </div>
+  </div>
+);
